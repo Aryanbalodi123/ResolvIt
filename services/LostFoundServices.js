e766e7547741e1d5900355ab2e264dc4c8db26ac
arunimasingh23 <arunima.2308@gmail.com>
2025-09-06 14:23:49 +0530

    Fixed Lost and Found

--- services/LostFoundServices.js
+++ services/LostFoundServices.js
@@ -1,11 +1,21 @@
-import { supabase } from "../lib/SupabaseClient";
+import { supabase } from "../lib/SupabaseClient"; 
 
-export async function reportLostItem(lostPayload) {
-    
-    const {data,error} = await supabase
-    .from('lost_found')
-    .insert([lostPayload])
+// Report a lost item
+export async function reportLostItem(payload) {
+  const { data, error } = await supabase
+    .from("lost_items") 
+    .insert([payload]);
 
-    if (error) throw error;
-    return data;
+  if (error) throw error;
+  return data;
+}
+
+// Report a found item
+export async function reportFoundItem(payload) {
+  const { data, error } = await supabase
+    .from("found_items") 
+    .insert([payload]);
+
+  if (error) throw error;
+  return data;
 }
